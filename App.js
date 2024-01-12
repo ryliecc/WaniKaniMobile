3fa75b3a646b19cb754782b6cf697049e8deee94
Rylie Castell <rylie.castell@gmail.com>
2024-01-12 23:49:32 +0100

    change example text

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        Welcome to the WaniKani Mobile App. It is still work in progress, but
+        have a look around if you want to. 😉
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });

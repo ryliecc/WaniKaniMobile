87a194ebfb95104ad852b02bc3d1ee373a1d58d0
Rylie Castell <rylie.castell@gmail.com>
2024-01-13 16:16:23 +0100

    add storage management

--- screens/TokenScreen.js
+++ screens/TokenScreen.js
@@ -1,10 +1,33 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { useState } from "react";
+import RNSecureStorage, { ACCESSIBLE } from "rn-secure-storage";
 
 export default function TokenScreen({ navigation }) {
+  const [text, setText] = useState("");
+
+  function saveToken() {
+    RNSecureStorage.setItem("api_token", text, {
+      accessible: ACCESSIBLE.ALWAYS,
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    navigation.goBack();
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Please enter your API token.</Text>
+      <TextInput
+        style={{ height: 40 }}
+        placeholder="Enter API token here."
+        onChangeText={(newText) => setText(newText)}
+        defaultValue={text}
+      />
+      <Button title="Done!" onPress={saveToken} />
       <Button title="Go back" onPress={() => navigation.goBack()} />
       <StatusBar style="auto" />
     </View>
